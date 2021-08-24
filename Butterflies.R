be9d74c93a7e3cb2dee2638bfc256b37da195aad
ginalamka <72942258+ginalamka@users.noreply.github.com>
2021-08-24 17:20:43 -0500

    edit and add LandscapeInit and FunctionSourcer
    this happened in class 8.24.2021 and is mimicked from Janna's script on her github. we created the landscape with a peak elevation at 400 and 150 x and y coordinates

---
+++ Butterflies.R
@@ -0,0 +1,28 @@
+#in ABM class 8.24.2021
+#the premise is that there are butterflies that go up in elevation
+
+setwd("C:/Users/HP/Box/New Computer/Auburn/Data/ABM_Class")
+directory = getwd()
+outdir = paste(directory, "/output/", sep = "")
+source(paste(directory, "/source/FunctionSourcer.R", sep = ''))
+
+#parameters
+elevation = c(0,400)      #peak elevation min and max
+landscape = 150           #number of patches on each side, total patch number = landscape*landscape
+
+nindv     = 50            #number of individuals to simulate
+nsteps    = 500           #number of steps an individual can take
+move      = 0.8           #decimal likelihood of individual moving to highest neighbor patch (R&G call this q)
+
+#init landscape
+land = LandscapeInit(elevation)
+image(land)
+
+#init individuals on landscape
+
+#allow individuals to move within landscape
+
+#extract needed output from simulation
+
+ 
+
---
+++ Source/FunctionSourcer.R
@@ -0,0 +1,11 @@
+#FunctionSourcer.R
+#this is for the butterfly project in the ABM class Fall 2021
+
+#set working directory, import packages, source functions,
+setwd(paste(directory,"/source/", sep = '')) #set temp working directory
+
+#import packages
+#library()
+
+#source functions
+source(paste(getwd(), "/LandscapeInit.R", sep = ''))
