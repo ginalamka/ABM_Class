04d8c3c1fa931782195685d0d8057a619c8abefe
ginalamka <72942258+ginalamka@users.noreply.github.com>
2021-09-10 14:45:02 -0500

    Update Butterflies.R

--- Butterflies.R
+++ Butterflies.R
@@ -7,50 +7,64 @@ outdir = paste(directory, "/output/", sep = "")
 source(paste(directory, "/source/FunctionSourcer.R", sep = ''))
 
 #parameters
-elevation = c(0,400)      #peak elevation min and max
-landscape = 150           #number of patches on each side, total patch number = landscape*landscape
+elevation.V = c(0,400)      #peak elevation min and max
+landscape.V = 150           #number of patches on each side, total patch number = landscape*landscape
 
-nindv     = 50            #number of individuals to simulate
-nsteps    = 500           #number of steps an individual can take
-move      = 0.8           #decimal likelihood of individual moving to highest neighbor patch (R&G call this q)
+nindv.V     = 50            #number of individuals to simulate
+nsteps.V    = 500           #number of steps an individual can take
+move.V      = 0.8           #decimal likelihood of individual moving to highest neighbor patch (R&G call this q)
+reps        = 5
 
-#pdf(file = "/Output/butterflies2.pdf", width = 5, height = 5, onefile = T)
-#init landscape
-land = LandscapeInit(elevation)
-image(land)
+parameters = expand.grid(elevation.V, landscape.V, nindv.V, nsteps.V, move.V)
+colnames(parameters) = c("elevation", "landscape", "number or individuals", "number of steps", "likelihood of moving")
+parameters = parameters[parameters$elevation!=0,]
 
-#init individuals on landscape
-pop = NewPop(nindv, landscape)
-points(pop[,1]/150, pop[,2]/150, pch=19, cex=0.5)
-dev.copy(png, "../Output/indvlocation.png")
-dev.off()
-
-#pop = rbind(pop,NewPop(nindv,landscape)) #this will add the different NewPops together
-#plot(-100,-100, xlim=c(0,150), ylim=c(0,150))  #this puts the points on its own figure (note 0-150 axes)
-#points(pop[,1], pop[,2], pch=19, cex=0.5) #puts points on own fig
-
-#allow individuals to move within landscape
-paths = NULL
-for(i in 1:nindv){
-  indv = pop[i,,drop = FALSE]
+for(p in 1:nrow(parameters)){
+  elevation = c(0, parameters$elevation[p])
+  landscape = parameters$landscape[p]
+  nindv     = parameters$nindv[p]
+  move      = parameters$move[p]
   
-  #chart movement
-  movepath = Move(nindv, land, move, nsteps, elevation)
-  
-  #plot movement
-  lines(movepath[seq(1,length(movepath), 2)]/150, movepath[seq(2,length(movepath), 2)]/150, lwd = 2)
-  
-  #record path in single object for all indv
-  paths = rbind(paths, movepath)
+  for(r in 1:reps){
+    #init landscape
+    land = LandscapeInit(elevation)
+    image(land)
+    
+    #init individuals on landscape
+    pop = NewPop(nindv, landscape)
+    points(pop[,1]/150, pop[,2]/150, pch=19, cex=0.5)
+    dev.copy(png, "../Output/indvlocation.png")
+    dev.off()
+    
+    #pop = rbind(pop,NewPop(nindv,landscape)) #this will add the different NewPops together
+    #plot(-100,-100, xlim=c(0,150), ylim=c(0,150))  #this puts the points on its own figure (note 0-150 axes)
+    #points(pop[,1], pop[,2], pch=19, cex=0.5) #puts points on own fig
+    
+    #allow individuals to move within landscape
+    paths = NULL
+    for(i in 1:nindv){
+      indv = pop[i,,drop = FALSE]
+      
+      #chart movement
+      movepath = Move(nindv, land, move, nsteps, elevation)
+      
+      #plot movement
+      lines(movepath[seq(1,length(movepath), 2)]/150, movepath[seq(2,length(movepath), 2)]/150, lwd = 2)
+      
+      #record path in single object for all indv
+      paths = rbind(paths, movepath)
+    }
+    
+    rownames(paths) = seq(1, nindv, 1)
+    
+    #extract needed output from simulation
+    #for this project, we will not be doing any stats, but export something (like a figure) to see how the model worked
+    dev.copy(png, "../Output/butterflypaths.png")
+    dev.off()
+    
+    #to write a table with the x, y coordinates use the code below
+    #write.table(pop, paste(directory, "/output/testpop2.txt", sep=""), sep="/t", col.names=F, row.names=F)
+    
+  }
+
 }
-
-rownames(paths) = seq(1, nindv, 1)
-
-#extract needed output from simulation
-#for this project, we will not be doing any stats, but export something (like a figure) to see how the model worked
-dev.copy(png, "../Output/butterflypaths.png")
-dev.off()
-
-#to write a table with the x, y coordinates use the code below
-#write.table(pop, paste(directory, "/output/testpop2.txt", sep=""), sep="/t", col.names=F, row.names=F)
-
